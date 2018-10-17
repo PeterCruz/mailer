53d134244c8caf6742519660381a9ee088341281
Pedro Cruz <pedro.cruz@blackinntech.com>
2018-10-16 21:26:06 -0500

    make it better

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router  = express.Router();
+const mailer = require('../helpers/mailer');
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.post('/', (req,res) => {
+  let options = req.body;
+  options.filename = 'verify';
+  mailer.send(options)
+      .then(() => {
+        res.status(200).send('el correo se mandó')
+      })
+      .catch(err => {
+        console.log('algo salió mal', err)
+      })
+});
+
+module.exports = router;
